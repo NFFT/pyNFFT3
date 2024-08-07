d53b01a5e9f8a809389d39be442b19253c2550ac
Michael Nolander <113628428+mnolander@users.noreply.github.com>
2024-08-07 10:52:10 +0200

    Reformatted Python files using black

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,26 +7,26 @@ import sys, os
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'pyNFFT3'
-copyright = '2024, Michael Nolander'
-author = 'Michael Nolander'
-release = '1.0.0'
+project = "pyNFFT3"
+copyright = "2024, Michael Nolander"
+author = "Michael Nolander"
+release = "1.0.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-sys.path.insert(0, os.path.abspath('../../src'))
+sys.path.insert(0, os.path.abspath("../../src"))
 print(sys.path)
 
-extensions = ['sphinx.ext.autodoc']
+extensions = ["sphinx.ext.autodoc"]
 
 autodoc_default_options = {
-    'members': True,
-    'undoc-members': True,
-    'private-members': True,
-    'special-members': '__init__',
-    'inherited-members': True,
-    'show-inheritance': True,
+    "members": True,
+    "undoc-members": True,
+    "private-members": True,
+    "special-members": "__init__",
+    "inherited-members": True,
+    "show-inheritance": True,
 }
 
 # ClassDocumenter.add_directive_header uses ClassDocumenter.add_line to
@@ -39,7 +39,8 @@ autodoc_default_options = {
 from sphinx.ext.autodoc import ClassDocumenter, _
 
 add_line = ClassDocumenter.add_line
-line_to_delete = _('Bases: %s') % ':py:class:`object`'
+line_to_delete = _("Bases: %s") % ":py:class:`object`"
+
 
 def add_line_no_object_base(self, text, *args, **kwargs):
     if text.strip() == line_to_delete:
@@ -47,7 +48,9 @@ def add_line_no_object_base(self, text, *args, **kwargs):
 
     add_line(self, text, *args, **kwargs)
 
+
 add_directive_header = ClassDocumenter.add_directive_header
+
 
 def add_directive_header_no_object_base(self, *args, **kwargs):
     self.add_line = add_line_no_object_base.__get__(self)
@@ -58,13 +61,14 @@ def add_directive_header_no_object_base(self, *args, **kwargs):
 
     return result
 
+
 ClassDocumenter.add_directive_header = add_directive_header_no_object_base
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'default'
-html_static_path = ['_static']
+html_theme = "default"
+html_static_path = ["_static"]
